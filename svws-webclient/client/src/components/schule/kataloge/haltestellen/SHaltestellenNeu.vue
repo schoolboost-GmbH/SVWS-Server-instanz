b05109510dc006698a8ac20a80061238f68c8115
Maxim Irmscher ]init[ <maxim.irmscher@init.de>
2025-09-12 13:25:35 +0000

    Resolve "Validierung Haltestellen"

--- svws-webclient/client/src/components/schule/kataloge/haltestellen/SHaltestellenNeu.vue
+++ svws-webclient/client/src/components/schule/kataloge/haltestellen/SHaltestellenNeu.vue
@@ -1,20 +1,25 @@
 <template>
 	<div class="page page-grid-cards">
 		<svws-ui-content-card title="Haltestelle anlegen">
-			<svws-ui-input-wrapper :grid="1">
-				<svws-ui-text-input placeholder="Bezeichnung" :max-len="30" :min-len="1" v-model="data.bezeichnung" :disabled :valid="fieldIsValid('bezeichnung')" />
-				<svws-ui-input-wrapper :grid="2">
-					<svws-ui-input-number placeholder="Entfernung zur Schule" v-model="data.entfernungSchule" :disabled :min="0" :max="32000" />
-					<svws-ui-input-number placeholder="Sortierung" v-model="data.sortierung" :disabled :min="0" :max="32000" />
-					<svws-ui-spacing />
-					<svws-ui-checkbox v-model="data.istSichtbar" :disabled>
-						Sichtbar
-					</svws-ui-checkbox>
-					<div class="mt-7 flex flex-row gap-4 justify end">
-						<svws-ui-button type="secondary" @click="cancel">Abbrechen</svws-ui-button>
-						<svws-ui-button @click="add" :disabled="!formIsValid || !hatKompetenzAdd">Speichern</svws-ui-button>
-					</div>
-				</svws-ui-input-wrapper>
+			<svws-ui-input-wrapper>
+				<svws-ui-text-input placeholder="Bezeichnung" :max-len="30" :min-len="1" v-model="data.bezeichnung" :disabled required
+					:valid="fieldIsValid('bezeichnung')" />
+				<div v-if="!isUniqueInList(data.bezeichnung, props.manager().liste.list(), 'bezeichnung')" class="flex my-auto">
+					<span class="icon i-ri-alert-line mx-0.5 mr-1 inline-flex" />
+					<p> Diese Bezeichnung wird bereits verwendet. </p>
+				</div>
+				<div v-if="bezeichnungIsTooLong" class="flex my-auto">
+					<span class="icon i-ri-alert-line mx-0.5 mr-1 inline-flex" />
+					<p> Diese Bezeichnung verwendet zu viele Zeichen. </p>
+				</div>
+				<svws-ui-input-number placeholder="Entfernung zur Schule" v-model="data.entfernungSchule" :disabled :min="0" :max="32000" />
+				<svws-ui-input-number placeholder="Sortierung" v-model="data.sortierung" :disabled :min="0" :max="32000" />
+				<svws-ui-spacing />
+				<svws-ui-checkbox v-model="data.istSichtbar" :disabled>Sichtbar</svws-ui-checkbox>
+				<div class="mt-7 flex flex-row gap-4 justify end">
+					<svws-ui-button type="secondary" @click="cancel">Abbrechen</svws-ui-button>
+					<svws-ui-button @click="add" :disabled="!formIsValid || !hatKompetenzAdd">Speichern</svws-ui-button>
+				</div>
 			</svws-ui-input-wrapper>
 		</svws-ui-content-card>
 		<svws-ui-checkpoint-modal :checkpoint :continue-routing="props.continueRoutingAfterCheckpoint" />
@@ -24,14 +29,22 @@
 <script setup lang="ts">
 
 	import type { HaltestellenNeuProps } from "~/components/schule/kataloge/haltestellen/SHaltestellenNeuProps";
-	import { BenutzerKompetenz, Haltestelle, JavaString } from "@core";
+	import { BenutzerKompetenz, Haltestelle } from "@core";
 	import { computed, ref, watch } from "vue";
+	import { isUniqueInList, mandatoryInputIsValid } from "~/util/validation/Validation";
 
 	const props = defineProps<HaltestellenNeuProps>();
-	const data = ref<Haltestelle>(Object.assign( new Haltestelle(), {istSichtbar: true, entfernungSchule: 0, sortierung: 1}))
+	const data = ref<Haltestelle>(Object.assign( new Haltestelle(), { istSichtbar: true, entfernungSchule: 0 }))
 	const isLoading = ref<boolean>(false);
 	const hatKompetenzAdd = computed<boolean>(() => props.benutzerKompetenzen.has(BenutzerKompetenz.KATALOG_EINTRAEGE_AENDERN));
 	const disabled = computed(() => !hatKompetenzAdd.value);
+
+	const bezeichnungIsTooLong = computed(() => {
+		if (data.value.bezeichnung === null)
+			return false;
+
+		return data.value.bezeichnung.length > 30;
+	});
 
 	function fieldIsValid(field: keyof Haltestelle | null) : (v: string | null) => boolean {
 		return (v: string | null) => {
@@ -53,14 +66,11 @@
 		})
 	})
 
-	function bezeichnungIsValid(v: string | null) {
-		if ((v === null) || JavaString.isBlank(v) || (v.length > 30))
+	function bezeichnungIsValid(value: string | null) {
+		if (!mandatoryInputIsValid(value, 30))
 			return false;
-		for (const haltestelle of props.manager().liste.list()) {
-			if (JavaString.equalsIgnoreCase(v, haltestelle.bezeichnung))
-				return false;
-		}
-		return true;
+
+		return isUniqueInList(value, props.manager().liste.list(), 'bezeichnung');
 	}
 
 	async function add() {
@@ -82,7 +92,8 @@
 	watch(() => data.value, async() => {
 		if (isLoading.value)
 			return;
+
 		props.checkpoint.active = true;
-	}, {immediate: false, deep: true});
+	}, { immediate: false, deep: true });
 
 </script>
