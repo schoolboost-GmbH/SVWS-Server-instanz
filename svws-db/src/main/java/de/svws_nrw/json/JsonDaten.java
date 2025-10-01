c0dba41c21f765fdfcf82d4f27c0cd16d14a3ad9
Anne Schüller <anne.schueller@svws-nrw.de>
2025-10-01 19:33:17 +0000

    Fehlende Fachklassen fürs BK ergänzt

--- svws-db/src/main/java/de/svws_nrw/json/JsonDaten.java
+++ svws-db/src/main/java/de/svws_nrw/json/JsonDaten.java
@@ -51,7 +51,7 @@ public final class JsonDaten {
 		final BerufskollegFachklassenKatalog katalog = new BerufskollegFachklassenKatalog();
 		final int[] indizes = {
 				10, 20, 30, 40, 50, 55, 56, 57, 58,
-				60, 61, 62, 63, 70, 80, 90, 91, 92, 93, 94,
+				60, 61, 62, 63, 65, 66, 67, 70, 80, 90, 91, 92, 93, 94,
 				100, 110, 120, 130, 140, 141, 145, 146,
 				150, 160, 170, 180, 190,
 				200, 210, 220, 230, 240, 250, 260, 270, 280, 290,
