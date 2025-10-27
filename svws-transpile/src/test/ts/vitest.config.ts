e14add7fb9a76697590d3222b322a9b79ce1ae73
ThomasBachran <thomas.bachran@gmx.de>
2025-10-27 14:05:13 +0100

    Bugfix Parallelisierung für vitest-Tests beim Transpiler deaktiviert

--- svws-transpile/src/test/ts/vitest.config.ts
+++ svws-transpile/src/test/ts/vitest.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'node:path';
 export default defineConfig({
 	test: {
 		globals: true,
+		pool: "threads",
 		reporters: ["default", "junit"],
 		outputFile: "../../../build/coverage/junit.xml",
 		coverage: {
