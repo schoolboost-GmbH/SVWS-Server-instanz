5fcb5b475838df1f0df58f67ccfaa4cbdcf845dd
Usama Saeed <93057308+usamasaeed1028@users.noreply.github.com>
2025-12-01 14:40:39 +0100

    fix:restoartion issue

--- deployment/docker/backup/backup.sh
+++ deployment/docker/backup/backup.sh
@@ -6,17 +6,18 @@ while true; do
   DATE=$(date +%Y-%m-%d_%H-%M-%S)
   echo "Backing up all databases..."
 
-  mysqldump -h mariadb -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" \
+  # Backup all databases AS ROOT
+  mysqldump -h mariadb -uroot -p"$MARIADB_ROOT_PASSWORD" \
             --all-databases --single-transaction --routines --triggers --events \
             --skip-lock-tables --column-statistics=0 \
             > /backups/all_databases_$DATE.sql
 
-  DBS=$(mysql -h mariadb -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" -sNe "SHOW DATABASES;" | grep -Ev '^(mysql|information_schema|performance_schema|sys)$')
+  DBS=$(mysql -h mariadb -uroot -p"$MARIADB_ROOT_PASSWORD" -sNe "SHOW DATABASES;" | grep -Ev '^(mysql|information_schema|performance_schema|sys)$')
 
   while IFS= read -r db; do
     OUT=$(echo "$db" | tr ' ' '_')
     echo "Backing up '$db' individually..."
-    mysqldump -h mariadb -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" \
+    mysqldump -h mariadb -uroot -p"$MARIADB_ROOT_PASSWORD" \
               --single-transaction --routines --triggers --events \
               --databases "$db" --skip-lock-tables --column-statistics=0 \
               > /backups/${OUT}_$DATE.sql
