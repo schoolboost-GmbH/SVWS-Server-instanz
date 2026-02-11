9afd9a0f68ea10e8f4f4a2a1d090dd42c7836e5e
Usama Saeed <93057308+usamasaeed1028@users.noreply.github.com>
2026-02-11 18:56:22 +0100

    fix: memory allocations

--- deployment/docker/svws/startup.sh
+++ deployment/docker/svws/startup.sh
@@ -65,7 +65,7 @@ if [[ -z "$JAR_FILE" ]]; then
     exit 1
 fi
 
-java -Xmx7g -Xms2g \
+java -Xmx6g -Xms2g \
     -XX:+ExitOnOutOfMemoryError \
     -XX:+HeapDumpOnOutOfMemoryError \
     -XX:HeapDumpPath=/opt/app/svws/logs/ \
